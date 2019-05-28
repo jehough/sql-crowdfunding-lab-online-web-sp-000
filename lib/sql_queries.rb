1d12125f397c029f0f665e1acd44d0b47a371858
Jim Hough <teachlife@outlook.com>
2019-05-28 01:01:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,6 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title, pledges.amount FROM pledges
   INNER JOIN projects ON pledges.project_id = projects.id
+  GROUP BY (projects.title)
   ORDER BY (projects.title);"
 end
 
