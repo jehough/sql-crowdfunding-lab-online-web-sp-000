82605ba7357ea865076e05fc346838920d1e6140
Jim Hough <teachlife@outlook.com>
2019-05-28 01:14:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ ORDER BY (users.name);"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.name, SUM(pledges.amount) - projects.funding_goal > 0 FROM pledges
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal > 0 FROM pledges
 INNER JOIN projects ON pledges.project_id = projects.id
 GROUP BY (projects.name)
 ORDER BY (projects.name); "
